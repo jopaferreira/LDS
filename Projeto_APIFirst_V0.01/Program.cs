3b8cb11cac0f93acfc79d795aa1973886f8bc517
jopaferreira <44246935+jopaferreira@users.noreply.github.com>
2024-05-05 21:12:07 +0000

    Teste API
    Código Simples para testar a API

---
+++ Projeto_APIFirst_V0.01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace UnirPDFs
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que unir quantos ficheiros PDF? :");
+            int numeroDeFicheiros = int.Parse(Console.ReadLine());
+
+            string[] ficheirosPDF = new string[numeroDeFicheiros];
+
+            for (int i = 0; i < numeroDeFicheiros; i++)
+            {
+                Console.WriteLine($"Introduza nome do {i + 1}º ficheiro PDF :");
+                ficheirosPDF[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Qual o nome do ficheiro final? :");
+            string ficheiroPDFSaida = Console.ReadLine();
+
+            UnirPDFs(ficheirosPDF, ficheiroPDFSaida);
+        }
+
+        static void UnirPDFs(string[] ficheirosPDF, string ficheiroPDFSaida)
+        {
+            PdfDocument documentoPDF = new PdfDocument();
+
+            foreach (string ficheiroPDF in ficheirosPDF)
+            {
+                PdfDocument pdf = PdfReader.Open(ficheiroPDF, PdfDocumentOpenMode.Import);
+
+                foreach (PdfPage pagina in pdf.Pages)
+                {
+                    documentoPDF.AddPage(pagina);
+                }
+            }
+
+            documentoPDF.Save(ficheiroPDFSaida);
+        }
+    }
+}
